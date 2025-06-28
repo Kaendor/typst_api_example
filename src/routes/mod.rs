a3589994fba217eea7946ae4427d8a29b82a789e
Kaendor <dimteboul@gmail.com>
2025-06-28 15:30:11 +0200

    feat(error): add better error handling

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,35 +1,39 @@
 use axum::{
     Json,
     http::{HeaderMap, header},
-    response::IntoResponse,
+    response::{IntoResponse, Result},
 };
 use tracing::{info, instrument};
-use typst_pdf_api::templates::german_invoice::GERMAN_INVOICE_TEMPLATE;
+use typst_pdf_api::templates::{AppError, german_invoice::GERMAN_INVOICE_TEMPLATE};
 
 // #[axum::debug_handler]
 #[instrument]
-pub async fn pdf_generation_controller(Json(_payload): Json<CreatePDF>) -> impl IntoResponse {
+pub async fn pdf_generation_controller(
+    Json(_payload): Json<CreatePDF>,
+) -> Result<impl IntoResponse> {
     info!("Serving PDF");
     // This is where you would implement the logic to convert a template to PDF.
     // For now, we return a simple string.
     let german_template = GERMAN_INVOICE_TEMPLATE;
 
-    let pdf_buf = typst_pdf_api::templates::template_to_pdf(german_template.to_string());
+    let pdf_buf = typst_pdf_api::templates::template_to_pdf(german_template.to_string())?;
 
     let mut headers = HeaderMap::new();
     headers.insert(
         header::CONTENT_TYPE,
-        "application/pdf".parse().expect("pdf header parse"),
+        "application/pdf"
+            .parse()
+            .map_err(|_| AppError::InternalServerError)?,
     );
     headers.insert(
         header::CONTENT_DISPOSITION,
         "inline; filename=\"output.pdf\""
             .parse()
-            .expect("filename header parse"),
+            .map_err(|_| AppError::InternalServerError)?,
     );
 
     info!("PDF Served");
-    (headers, pdf_buf)
+    Ok((headers, pdf_buf))
 }
 
 #[derive(serde::Deserialize, serde::Serialize, Debug)]
--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -1,3 +1,10 @@
+use axum::{
+    extract::FromRequest,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+use serde::Serialize;
+use thiserror::Error;
 use tracing::instrument;
 use typst::{diag::Warned, layout::PagedDocument};
 use typst_pdf::PdfOptions;
@@ -6,8 +13,18 @@ use crate::TypstWrapperWorld;
 
 pub mod german_invoice;
 
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error("Failed to compile template")]
+    CompilationError,
+    #[error("PDF generation error")]
+    PdfGenerationError,
+    #[error("Internal server error")]
+    InternalServerError,
+}
+
 #[instrument]
-pub fn template_to_pdf(content: String) -> Vec<u8> {
+pub fn template_to_pdf(content: String) -> Result<Vec<u8>, AppError> {
     // This function is a placeholder for the actual implementation.
     // It should return the path or content of the PDF generated from the template.
     let world = TypstWrapperWorld::new("./examples".to_owned(), content);
@@ -17,18 +34,61 @@ pub fn template_to_pdf(content: String) -> Vec<u8> {
         warnings: _warnings,
     } = typst::compile::<PagedDocument>(&world);
 
-    let document = output.expect("Failed to compile template");
+    let document = output.map_err(|_| AppError::CompilationError)?;
 
-    let pdf_buf = typst_pdf::pdf(&document, &PdfOptions::default()).expect("PDF Generation");
+    let pdf_buf = typst_pdf::pdf(&document, &PdfOptions::default())
+        .map_err(|_| AppError::PdfGenerationError)?;
 
-    pdf_buf
+    Ok(pdf_buf)
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        // How we want errors responses to be serialized
+        #[derive(Serialize)]
+        struct ErrorResponse {
+            message: String,
+        }
+
+        let (status, message) = match self {
+            AppError::CompilationError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Oopsie doopsie".to_owned(),
+            ),
+            AppError::PdfGenerationError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "Something went wrong".to_owned(),
+            ),
+            AppError::InternalServerError => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "An internal server error occurred".to_owned(),
+            ),
+        };
+
+        tracing::error!("PDF generation error: {}", message);
+
+        (status, AppJson(ErrorResponse { message })).into_response()
+    }
+}
+
+#[derive(FromRequest)]
+#[from_request(via(axum::Json), rejection(AppError))]
+struct AppJson<T>(T);
+
+impl<T> IntoResponse for AppJson<T>
+where
+    axum::Json<T>: IntoResponse,
+{
+    fn into_response(self) -> Response {
+        axum::Json(self.0).into_response()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn pdf_generation_test() {
-        let pdf_buf = super::template_to_pdf("Hello, Typst!".to_string());
+        let pdf_buf = super::template_to_pdf("Hello, Typst!".to_string()).expect("pdf gen");
 
         assert!(!pdf_buf.is_empty(), "PDF buffer should not be empty");
         assert!(
